1b65c0da3c7f11b140f93813f09c1a534052a50c
PavlivAndriy <andriypavliv1@gmail.com>
2016-01-04 17:56:59 +0200

    Initial commit

---
+++ src/main/java/Second/App.java
@@ -0,0 +1,25 @@
+package Second;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+
+        FIRST_LOOP : for (int i = 0; i < 10; i++){
+            System.out.println("Number is " + i);
+            if (i == 7){
+                continue FIRST_LOOP;
+            } else if (i == 8){
+                break FIRST_LOOP;
+            }
+        }
+        System.out.println("Some changes");
+
+    }
+}
